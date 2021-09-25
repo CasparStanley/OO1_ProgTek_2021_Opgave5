0b82998a4afb319ab55f68559b3bddf6c5e24138
Caspar Stanley <casparstan@gmail.com>
2021-09-25 14:52:08 +0200

    TCP server is up and running with Football Player GetAll and Get(ID) - working on Save

---
+++ OO1_5_TCP-Server_FootballPlayer/OO1_5_TCP-Client/Client.cs
@@ -0,0 +1,42 @@
+﻿using OO1_Opgave1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO1_5_TCP_Client
+{
+    class Client
+    {
+        public void Start()
+        {
+            Console.WriteLine("COMMANDS:\n'HentAlle', Enter x2   - Henter Alle Objekter" +
+                              "\n'Hent', Enter, '<ID>', Enter   - Henter et specifikt objekt" +
+                              "\n'Gem', Enter, '<JSON Objekt>' (eks. 'ID':123, ...), Enter   - Gemmer et objekt" +
+                              "\n\n");
+
+            TcpClient socket = new TcpClient("localhost", 2121);
+
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                sw.AutoFlush = true;
+
+                string output1 = Console.ReadLine();
+                sw.WriteLine(output1);
+
+                string output2 = Console.ReadLine();
+                sw.WriteLine(output2);
+
+                string line1 = sr.ReadLine();
+                Console.WriteLine(line1);
+
+                string line2 = sr.ReadLine();
+                Console.WriteLine(line2);
+            }
+        }
+    }
+}
---
+++ OO1_5_TCP-Server_FootballPlayer/OO1_5_TCP-Server_FootballPlayer/Server.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Text.Json;
+using OO1_Opgave1;
+using System.Threading;
+
+namespace OO1_5_TCP_Server
+{
+    class Server
+    {
+        public Server() { }
+
+        public void Start(int port)
+        {
+            TcpListener listener = new TcpListener(port);
+            listener.Start();
+
+            // While loop to handle more than a single client
+            while(true)
+            {
+                TcpClient socket = listener.AcceptTcpClient();
+
+                // Task to handle concurrent clients, on separate threads
+                Task.Run(() =>
+                {
+                    // Doing this to ensure loop is not continued before DoClient is done
+                    TcpClient tempSocket = socket;
+                    DoClient(tempSocket);
+                }
+                );
+            }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            // "using" makes sure it is only opened here, and automatically closed when done
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                // SETUP ------------------------------------------------------------------------------------- SETUP
+                string commandOne = sr.ReadLine();
+                string commandTwo = sr.ReadLine();
+
+                // INPUT ------------------------------------------------------------------------------------- INPUT
+                if (commandOne.ToLower() == "hentalle")
+                {
+                    string output = GetAll();
+                    sw.WriteLine(output);
+                    Console.WriteLine(output);
+                }
+
+                else if (commandOne.ToLower() == "hent")
+                {
+                    string output = Get(commandTwo);
+                    sw.WriteLine(output);
+                    Console.WriteLine(output);
+                }
+
+                else if (commandOne.ToLower() == "gem")
+                {
+                    string output = Save(commandTwo);
+                    sw.WriteLine(output);
+                    Console.WriteLine(output);
+
+                    Console.WriteLine("\nGemmer Fodboldspiller\n");
+
+                    if (commandTwo.ToLower() == "0")
+                    {
+                        Console.WriteLine("\nGemmer Fodboldspiller 0\n");
+                    }
+                }
+
+                //
+                //Console.WriteLine($"Received: {player.ToString()}");
+            }
+
+            socket?.Close();
+        }
+
+        private string GetAll()
+        {
+            string allPlayers = JsonSerializer.Serialize(Program.players);
+
+            Console.WriteLine($"Henter Alle Fodboldspillere:");
+
+            // Split JSON into different lines
+            // - Works perfectly for the server,
+            // but sw.WriteLine only returns the first two FootballPlayers !WHEN RUNNING IN VISUAL STUDIO!
+            // It works fine in SocketTest. Very weird.
+            // You can try it if you like: 
+
+            /*
+            string output = "";
+            foreach (string s in SplitString(allPlayers, "},{"))
+            {
+                output += s + "\n";
+            }
+
+            return output;
+            */
+
+            return allPlayers;
+        }
+
+        private string Get(string id_string)
+        {
+            int id;
+            string errorMsg = "\nID var ikke et validt nummer. Afbryder.\n";
+
+            Console.WriteLine("\nHenter specifik Fodboldspiller...\n");
+
+            try
+            {
+                id = Int32.Parse(id_string);
+
+                return JsonSerializer.Serialize(Program.players.Find(i => i.Id == id));
+            }
+            catch
+            {
+                return errorMsg;
+            }
+        }
+
+        private string Save(string commandTwo)
+        {
+            string errorMsg = "\nKommandoen var ikke formateret korrekt. Fejlet.\n";
+
+            Console.WriteLine("\nGemmer Fodboldspiller\n");
+
+            try
+            {
+                Program.players.Add(JsonSerializer.Deserialize<FootballPlayer>(commandTwo));
+                return "Success!";
+            }
+            catch
+            {
+                return errorMsg;
+            }
+        }
+
+        private string[] SplitString(string input, string delimiter)
+        {
+            return input.Split(delimiter);
+        }
+    }
+}
